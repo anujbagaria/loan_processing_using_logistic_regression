264a92f24af16aedd03eab07fa3d9cfc745034ba
anujbagaria <bagariaanuj@yahoo.com>
2022-02-12 23:58:11 +0530

    Add files via upload

---
+++ loan_processing_using_logistic_regression.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 12 18:32:09 2022
+
+@author: bagar
+"""
+
+import pandas as pd
+
+#reading the data file
+prime_dataset = pd.read_csv('loan_data.csv')
